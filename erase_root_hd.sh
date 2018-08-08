1e900f07d2c9514701ee44aa8dc22453c63322be
Keaton Burleson <keaton.burleson@me.com>
2018-08-08 14:16:41 -0500

    Update erase_root_hd.sh

--- erase_root_hd.sh
+++ erase_root_hd.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 ## PUT ME IN /Library/Scripts ##
-diskutil apfs deleteContainer disk1
+#!/bin/sh
+echo "Unmounting disk /dev/disk0"
+diskutil unmountDisk /dev/disk0
+echo "Unmounted disk successfully"
+if diskutil apfs deleteContainer disk1 ; then
+    echo "Successfully removed apfs container"
+else
+    echo "Unable to remove apfs container..erasing anyway"
+fi
+
+echo "Erasing /dev/disk0"
 diskutil eraseDisk JHFS+ Macintosh\ HD /dev/disk0
+echo "New name for /dev/disk0: Macintosh HD"
+exit 0
